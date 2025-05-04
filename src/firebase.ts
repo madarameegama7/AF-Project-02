ca4b0adea4a54d71d0d9c1f0e84b5f1601a7722a
Madara Meegama <madarameegama7@gmail.com>
2025-05-04 15:17:45 +0530

    Stop tracking .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCstyAaduamK6Hof6sF41YTSDvF58yoLhY",
-  authDomain: "explorenation-d16e0.firebaseapp.com",
-  projectId: "explorenation-d16e0",
-  storageBucket: "explorenation-d16e0.firebasestorage.app",
-  messagingSenderId: "592747877083",
-  appId: "1:592747877083:web:7d88a187770b29639c72fe",
-  measurementId: "G-2K01E8RLN3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
